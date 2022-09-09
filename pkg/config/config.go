be551de59f1c61656d7372ad4e75d966d55ba89c
Rodrigo <95635797+poly-rodr@users.noreply.github.com>
2022-09-09 10:17:48 -0300

    updating contracts

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -17,22 +17,22 @@ type ClobContracts struct {
 var (
 	MUMBAI_CONTRACTS = &ClobContracts{
 		Exchange: &Exchange{
-			Address: "0x6D486b31b5c0f724828Aff07c88606b213B0D196",
+			Address: "0xA5caFCC00E8D8E9121CC18B2DF279Eab5dE43bC5",
 			Name:    "Polymarket Limit Order Protocol",
 			Version: "1",
 		},
-		Executor:    "0x6b0ab7A1E65ea6AE9072f6c45B4261ACDfB30827",
+		Executor:    "0xb2a29463Df393a4CAef36541544715e6B48b80B7",
 		Collateral:  "0x2E8DCfE708D44ae2e406a1c02DFE2Fa13012f961",
 		Conditional: "0x7D8610E9567d2a6C9FBf66a5A13E9Ba8bb120d43",
 	}
 
 	MATIC_CONTRACTS = &ClobContracts{
 		Exchange: &Exchange{
-			Address: "0x6D486b31b5c0f724828Aff07c88606b213B0D196",
+			Address: "0xA5caFCC00E8D8E9121CC18B2DF279Eab5dE43bC5",
 			Name:    "Polymarket Limit Order Protocol",
 			Version: "1",
 		},
-		Executor:    "0x6b0ab7A1E65ea6AE9072f6c45B4261ACDfB30827",
+		Executor:    "0xb2a29463Df393a4CAef36541544715e6B48b80B7",
 		Collateral:  "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
 		Conditional: "0x4D97DCd97eC945f40cF65F87097ACe5EA0476045",
 	}
