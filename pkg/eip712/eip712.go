7a59551c5b995905d5ba83a82faeb8954207d78e
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2023-05-19 15:50:57 -0400

    fix: add validate sig to go order utils pkg

--- pkg/eip712/eip712.go
+++ pkg/eip712/eip712.go
@@ -1,12 +1,14 @@
 package eip712
 
 import (
+	"bytes"
 	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 func BuildEIP712DomainSeparator(name, version common.Hash, chainId *big.Int, address common.Address) (common.Hash, error) {
@@ -34,4 +36,31 @@ func HashTypedDataV4(domainSeparator common.Hash, args []abi.Type, values []inte
 
 	rawData := []byte(fmt.Sprintf("\x19\x01%s%s", string(domainSeparator[:]), string(crypto.Keccak256Hash(encoded).Bytes())))
 	return crypto.Keccak256Hash(rawData), nil
+}
+
+// Verifies an EIP712 signature
+func ValidateSignature(signer common.Address, hashedData common.Hash, signature []byte) (bool, error) {
+	sigCopy := make([]byte, len(signature))
+	copy(sigCopy, signature)
+
+	if len(sigCopy) != 65 {
+		return false, secp256k1.ErrInvalidSignatureLen
+	}
+
+	if sigCopy[64] != 0 && sigCopy[64] != 1 { // in case of ledger signing v might already be 0 or 1
+		sigCopy[64] -= 27 // Transform V from 27/28 to 0/1 according to the yellow paper
+	}
+
+	sigPublicKey, err := crypto.Ecrecover(hashedData.Bytes(), sigCopy)
+	if err != nil {
+		return false, err
+	}
+
+	recoveredPublicKey, err := crypto.UnmarshalPubkey(sigPublicKey)
+	if err != nil {
+		return false, err
+	}
+
+	recoveredAddress := crypto.PubkeyToAddress(*recoveredPublicKey)
+	return bytes.Equal(signer.Bytes(), recoveredAddress.Bytes()), nil
 }
