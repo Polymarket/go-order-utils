0a5be65a9a86e101bddf4c7418e167cc236459cb
Rodrigo <95635797+poly-rodr@users.noreply.github.com>
2024-04-11 12:14:44 -0300

    migration to amoy (#14)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -18,14 +18,14 @@ type Contracts struct {
 
 func GetContracts(chainId int64) (*Contracts, error) {
 	var (
-		MUMBAI_CONTRACTS = &Contracts{
-			Exchange:         common.HexToAddress("0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E"),
-			FeeModule:        common.HexToAddress("0x56C79347e95530c01A2FC76E732f9566dA16E113"),
-			NegRiskExchange:  common.HexToAddress("0x87d1A0DdB4C63a6301916F02090A51a7241571e4"),
-			NegRiskFeeModule: common.HexToAddress("0x311FD94978961EeF4B903519B33B47B506aB6823"),
-			NegRiskAdapter:   common.HexToAddress("0x9A6930BB811fe3dFE1c35e4502134B38EC54399C"),
-			Collateral:       common.HexToAddress("0x2E8DCfE708D44ae2e406a1c02DFE2Fa13012f961"),
-			Conditional:      common.HexToAddress("0x7D8610E9567d2a6C9FBf66a5A13E9Ba8bb120d43"),
+		AMOY_CONTRACTS = &Contracts{
+			Exchange:         common.HexToAddress("0xdFE02Eb6733538f8Ea35D585af8DE5958AD99E40"),
+			FeeModule:        common.HexToAddress("0x9A9faEf45C671cc57B3e117c5B3053075416490f"),
+			NegRiskExchange:  common.HexToAddress("0xC5d563A36AE78145C45a50134d48A1215220f80a"),
+			NegRiskFeeModule: common.HexToAddress("0x78769d50be1763ed1ca0d5e878d93f05aabff29e"),
+			NegRiskAdapter:   common.HexToAddress("0xd91E80cF2E7be2e162c6513ceD06f1dD0dA35296"),
+			Collateral:       common.HexToAddress("0x9c4e1703476e875070ee25b56a58b008cfb8fa78"),
+			Conditional:      common.HexToAddress("0x69308FB512518e39F9b16112fA8d994F4e2Bf8bB"),
 		}
 
 		MATIC_CONTRACTS = &Contracts{
@@ -42,8 +42,8 @@ func GetContracts(chainId int64) (*Contracts, error) {
 	switch chainId {
 	case 137:
 		return MATIC_CONTRACTS, nil
-	case 80001:
-		return MUMBAI_CONTRACTS, nil
+	case 80002:
+		return AMOY_CONTRACTS, nil
 	default:
 		return nil, fmt.Errorf("invalid chain id")
 	}
--- pkg/utils/module_test.go
+++ pkg/utils/module_test.go
@@ -10,14 +10,14 @@ import (
 )
 
 func TestGetVerifyingContractAddress(t *testing.T) {
-	// mumbai
-	contract, err := GetVerifyingContractAddress(big.NewInt(80001), model.CTFExchange)
+	// amoy
+	contract, err := GetVerifyingContractAddress(big.NewInt(80002), model.CTFExchange)
 	assert.NoError(t, err)
-	assert.Equal(t, common.HexToAddress("0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E").Hex(), contract.Hex())
+	assert.Equal(t, common.HexToAddress("0xdFE02Eb6733538f8Ea35D585af8DE5958AD99E40").Hex(), contract.Hex())
 
-	contract, err = GetVerifyingContractAddress(big.NewInt(80001), model.NegRiskCTFExchange)
+	contract, err = GetVerifyingContractAddress(big.NewInt(80002), model.NegRiskCTFExchange)
 	assert.NoError(t, err)
-	assert.Equal(t, common.HexToAddress("0x87d1A0DdB4C63a6301916F02090A51a7241571e4").Hex(), contract.Hex())
+	assert.Equal(t, common.HexToAddress("0xC5d563A36AE78145C45a50134d48A1215220f80a").Hex(), contract.Hex())
 
 	// polygon mainnet
 	contract, err = GetVerifyingContractAddress(big.NewInt(137), model.CTFExchange)
