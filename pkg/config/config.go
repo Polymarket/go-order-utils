d581ec47f15cce0482a6b335b11497f6b1f99fc2
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2025-07-30 15:46:30 -0400

    feat: amoy fee module v2

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -19,7 +19,7 @@ type Contracts struct {
 var (
 	_AMOY_CONTRACTS = &Contracts{
 		Exchange:         common.HexToAddress("0xdFE02Eb6733538f8Ea35D585af8DE5958AD99E40"),
-		FeeModule:        common.HexToAddress("0x9A9faEf45C671cc57B3e117c5B3053075416490f"),
+		FeeModule:        common.HexToAddress("0x6144986D3dd85fF13DE54EeB13cFf869F1c24ceD"),
 		NegRiskExchange:  common.HexToAddress("0xC5d563A36AE78145C45a50134d48A1215220f80a"),
 		NegRiskFeeModule: common.HexToAddress("0x78769d50be1763ed1ca0d5e878d93f05aabff29e"),
 		NegRiskAdapter:   common.HexToAddress("0xd91E80cF2E7be2e162c6513ceD06f1dD0dA35296"),
